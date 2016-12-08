14e76fe9a08e82d6d051da9d64615ad0d8a55842
iliyan <iliyan.popov93@gmail.com>
2016-12-08 19:12:10 +0200

    ForumDBContext class created

---
+++ TeamProject-Forum-ASP-NET/Entities/ForumDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TeamProject_Forum_ASP_NET.Models;
+
+namespace TeamProject_Forum_ASP_NET.Entities
+{
+
+    public class ForumDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ForumDBContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ForumDBContext Create()
+        {
+            return new ForumDBContext();
+        }
+    }
+
+}
