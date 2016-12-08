f4e3c19a6c23a36365cdd4e2daa2d5260760ce31
iliyan <iliyan.popov93@gmail.com>
2016-12-08 18:53:59 +0200

    Project Skeleton

---
+++ TeamProject-Forum-ASP-NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamProject_Forum_ASP_NET.MvcApplication" Language="C#" %>
---
+++ TeamProject-Forum-ASP-NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamProject_Forum_ASP_NET.Startup))]
+namespace TeamProject_Forum_ASP_NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
