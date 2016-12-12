b683267f78551a477894ba19be0c5e678f261500
iliyan <iliyan.popov93@gmail.com>
2016-12-12 14:05:43 +0200

    Automatic Migrations enabled

---
+++ TeamProject-Forum-ASP-NET/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace TeamProject_Forum_ASP_NET.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<TeamProject_Forum_ASP_NET.Entities.ForumDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "TeamProject_Forum_ASP_NET.Entities.ForumDBContext";
+        }
+
+        protected override void Seed(TeamProject_Forum_ASP_NET.Entities.ForumDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
