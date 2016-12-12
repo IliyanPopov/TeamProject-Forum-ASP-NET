f51ba9da95c4dcd384b4e9c4577da33e448bc023
iliyan <iliyan.popov93@gmail.com>
2016-12-12 16:32:22 +0200

    Category and UserControllers added

---
+++ TeamProject-Forum-ASP-NET/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamProject_Forum_ASP_NET.Entities;
+
+namespace TeamProject_Forum_ASP_NET.Controllers.Admin
+{
+    public class CategoryController : Controller
+    {
+        private ForumDBContext db = new ForumDBContext();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
