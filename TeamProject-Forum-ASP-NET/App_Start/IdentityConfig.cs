d400fc493bd98cd5dec6084e82ed3663755e791e
iliyan <iliyan.popov93@gmail.com>
2016-12-12 13:52:05 +0200

    Minor fixes

--- TeamProject-Forum-ASP-NET/App_Start/IdentityConfig.cs
+++ TeamProject-Forum-ASP-NET/App_Start/IdentityConfig.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using TeamProject_Forum_ASP_NET.Entities;
 using TeamProject_Forum_ASP_NET.Models;
 
 namespace TeamProject_Forum_ASP_NET
@@ -42,7 +43,7 @@ namespace TeamProject_Forum_ASP_NET
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ForumDBContext>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
@@ -53,11 +54,11 @@ namespace TeamProject_Forum_ASP_NET
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 1,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
